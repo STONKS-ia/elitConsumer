d095fce3b2b3eb8cc9df9146ccbb90de59ae22a0
alineskamo <lineskity@gmail.com>
2021-11-08 10:22:13 -0300

    :tada: First commit

---
+++ src/main/java/com/br/elit/elitConsumer/ElitConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.br.elit.elitConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElitConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElitConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/elit/elitConsumer/ElitConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.elit.elitConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElitConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
