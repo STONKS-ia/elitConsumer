eaee31cdd0612ec288fdd658d28848d743cdbfbd
S3nna <matheus9senna@hotmail.com>
2021-11-19 23:38:33 -0300

    last commit of api

--- src/test/java/com/br/elit/tests/ConsumerRepoTest.java
+++ src/test/java/com/br/elit/tests/ConsumerRepoTest.java
@@ -2,27 +2,23 @@ package com.br.elit.tests;
 
 import com.br.elit.elitConsumer.ElitConsumerApplication;
 import com.br.elit.elitConsumer.model.OceanModel;
+import com.br.elit.elitConsumer.model.ReportModel;
 import com.br.elit.elitConsumer.repository.OceanRepository;
-import org.aspectj.lang.annotation.Before;
+import com.br.elit.elitConsumer.repository.ReportRepository;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
-@DataJpaTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes= ElitConsumerApplication.class)
 public class ConsumerRepoTest {
@@ -30,6 +26,8 @@ public class ConsumerRepoTest {
     @Mock
     public OceanRepository oceanRepository;
 
+    @Mock
+    public ReportRepository reportRepository;
     @Test
     public void successTestSaveOcean() {
 
@@ -38,6 +36,22 @@ public class ConsumerRepoTest {
 
         // THEN findById(Mockito.anyLong())).thenReturn(Optional.of(oceanModel)
         Assertions.assertEquals("Teste_Ocean", oceanRepository.findById(1L).get().getNameOcean());
+
+
+    }
+
+
+    @Test
+    public void successTestReportModel() {
+
+        OceanModel oceanModel = new OceanModel(2, "Teste2-Ocean");
+        // GIVEN
+        reportRepository.save(new ReportModel("teste", oceanModel));
+
+        // THEN
+        Assertions.assertEquals("teste", reportRepository.findById(1L).get().getName());
+
+
     }
 
 
